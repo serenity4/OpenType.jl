a8110a38623d3fb59799316e3c136e19bb753eee
serenity4 <cedric.bel@hotmail.fr>
2021-06-16 22:36:26 +0200

    Make tests pass in CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,11 @@
 using OpenType
 using Test
 
-OpenTypeFont("test/resources/arial.ttf")
-OpenTypeFont("test/resources/JuliaMono-Regular.ttf")
+const arial = joinpath(@__DIR__, "resources", "arial.ttf")
+const juliamono = joinpath(@__DIR__, "resources", "JuliaMono-Regular.ttf")
+
+OpenTypeFont(arial)
+OpenTypeFont(juliamono)
 
 @testset "OpenType.jl" begin
     # Write your tests here.
