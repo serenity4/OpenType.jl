6d044fb2e6c36bf138377862e6b21663c70abae1
serenity4 <cedric.bel@hotmail.fr>
2021-07-18 22:16:36 +0200

    Add glyph extraction utilities

--- src/OpenType.jl
+++ src/OpenType.jl
@@ -2,12 +2,15 @@ module OpenType
 
 using Dates
 using SwapStreams
+using GeometryExperiments
 
 import Base: ==, isless, &, |, in, xor
 
 include("bitmasks.jl")
 include("types.jl")
 include("parser.jl")
+
+include("extract.jl")
 
 export
         OpenTypeFont,
