5d15489b07810e96f4001accd03d9a466a32c052
serenity4 <cedric.bel@hotmail.fr>
2024-09-09 13:20:14 +0200

    Bump version

--- src/shaping.jl
+++ src/shaping.jl
@@ -10,6 +10,9 @@ end
 
 ShapingOptions(script, language, direction::Direction = DIRECTION_LEFT_TO_RIGHT; enabled_features = Tag4[], disabled_features = Tag4[]) = ShapingOptions(script, language, direction, Set(@something(enabled_features, Tag4[])), Set(@something(disabled_features, Tag4[])))
 
+include("shaping/harfbuzz.jl")
+include("shaping/legacy.jl")
+
 function shape(font::OpenTypeFont, text, options::ShapingOptions)
   # XXX: If we don't have a font file, we could create a Freetype font
   # when reading from the IO and pass that into HarfBuzz.
